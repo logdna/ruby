00d652c33d274b3ad7aaefe9d4b7d518b7f864e6
vilyapilya <13.veter@gmail.com>
2019-09-05 16:41:50 -0700

    add rubycop config file and run it

--- lib/logdna/client.rb
+++ lib/logdna/client.rb
@@ -1,13 +1,13 @@
-require 'net/http'
-require 'socket'
-require 'json'
-require 'concurrent'
-require 'thread'
-require 'date'
+# frozen_string_literal: true
+
+require "net/http"
+require "socket"
+require "json"
+require "concurrent"
+require "date"
 
 module Logdna
   class Client
-
     def initialize(request, uri, opts)
       @uri = uri
 
@@ -27,98 +27,104 @@ module Logdna
       @retry_timeout = opts[:retry_timeout] ||= Resources::RETRY_TIMEOUT
     end
 
-    def process_message(msg, opts={})
-      processedMessage = {
+    def process_message(msg, opts = {})
+      processed_message = {
         line: msg,
         app: opts[:app],
         level: opts[:level],
         env: opts[:env],
         meta: opts[:meta],
-        timestamp: Time.now.to_i,
+        timestamp: Time.now.to_i
       }
-      processedMessage.delete(:meta) if processedMessage[:meta].nil?
-      processedMessage
+      processed_message.delete(:meta) if processed_message[:meta].nil?
+      processed_message
     end
 
     def schedule_flush
       @flush_scheduled = true
-      def start_timer
+      start_timer = lambda {
         sleep(@exception_flag ? @retry_timeout : @flush_interval)
         flush
-      end
-      thread = Thread.new{ start_timer }
+      }
+      thread = Thread.new { start_timer }
       thread.join
     end
 
     def write_to_buffer(msg, opts)
       if @lock.try_lock
-          if !@side_messages.empty?
-            @buffer.concat(@side_messages)
-          end
-          processed_message = process_message(msg, opts)
-          new_message_size = processed_message.to_s.bytesize
-          @buffer_byte_size += new_message_size
+        @buffer.concat(@side_messages) unless @side_messages.empty?
+        processed_message = process_message(msg, opts)
+        new_message_size = processed_message.to_s.bytesize
 
-          if @flush_limit > (new_message_size + @buffer_byte_size)
-             @buffer.push(processed_message)
-          else
-             @buffer.push(processed_message)
-             self.flush
-          end
-          @lock.unlock if @lock.locked?
+        @buffer_byte_size += new_message_size
+        @buffer.push(processed_message)
 
-          schedule_flush() if !@flush_scheduled
+        @lock.unlock if @lock.locked?
+
+        flush if @flush_limit <= @buffer_byte_size
+
+        schedule_flush unless @flush_scheduled
       else
-          @side_messages.push(process_message(msg, opts))
+        @side_messages.push(process_message(msg, opts))
+      end
+    end
+
+    def send_request
+      @request.body = {
+        e: "ls",
+        ls: @buffer.concat(@side_messages)
+      }.to_json
+      @side_messages.clear
+
+      begin
+        @response = Net::HTTP.start(
+          @uri.hostname,
+          @uri.port,
+          use_ssl: @uri.scheme == "https"
+        ) do |http|
+          http.request(@request)
+        end
+
+        if @response.is_a?(Net::HTTPForbidden)
+          puts "Please provide a valid ingestion key"
+        elsif !@response.is_a?(Net::HTTPSuccess)
+          puts "The response is not successful "
+        end
+
+        @exception_flag = false
+      rescue SocketError
+        puts "Network connectivity issue"
+        @exception_flag = true
+        @side_messages.concat(@buffer)
+      rescue Errno::ECONNREFUSED => e
+        puts "The server is down. #{e.message}"
+        @exception_flag = true
+        @side_messages.concat(@buffer)
+      rescue Timeout::Error => e
+        puts "Timeout error occurred. #{e.message}"
+        @exception_flag = true
+        @side_messages.concat(@buffer)
+      ensure
+        @buffer.clear
+        @lock.unlock if @lock.locked?
       end
     end
 
     def flush
+      @flush_scheduled = false
       return if @buffer.empty?
+
       if @lock.try_lock
-        @request.body = {
-          e: 'ls',
-          ls: @buffer.concat(@side_messages),
-        }.to_json
-        @side_messages.clear
-        @flush_scheduled = false
-
-        begin
-          @response = Net::HTTP.start(@uri.hostname, @uri.port, use_ssl: @uri.scheme == 'https') do |http|
-            http.request(@request)
-          end
-
-          if(@response.is_a?(Net::HTTPForbidden))
-            p "Please provide a valid ingestion key"
-          elsif(!@response.is_a?(Net::HTTPSuccess))
-            p "The response is not successful #{}"
-          end
-          @exception_flag = false
-        rescue SocketError
-          p "Network connectivity issue"
-          @exception_flag = true
-          @side_messages.concat(@buffer)
-        rescue Errno::ECONNREFUSED => e
-          puts "The server is down. #{e.message}"
-          @exception_flag = true
-          @side_messages.concat(@buffer)
-        rescue Timeout::Error => e
-          puts "Timeout error occurred. #{e.message}"
-          @exception_flag = true
-          @side_messages.concat(@buffer)
-        ensure
-          @buffer.clear
-          @lock.unlock if @lock.locked?
-        end
+        send_request
+      else
+        schedule_flush
       end
-   end
+    end
 
-   def exitout
-      if @buffer.any?
-        flush()
-      end
-        puts "Exiting LogDNA logger: Logging remaining messages"
-      return
+    def exitout
+      flush if @buffer.any?
+      puts "Exiting LogDNA logger: Logging remaining messages"
+      nil
     end
   end
 end
