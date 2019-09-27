ad1b1a4b9f659853027bb163baf3ad3681efcddb
vilyapilya <13.veter@gmail.com>
2019-09-27 14:01:23 -0700

    address comments

--- lib/logdna.rb
+++ lib/logdna.rb
@@ -129,5 +129,11 @@ module Logdna
         @client.exitout
       end
     end
+
+    def close
+      if defined? @client and !@client.nil?
+          @client.exitout()
+      end
+    end
   end
 end
--- lib/logdna/client.rb
+++ lib/logdna/client.rb
@@ -35,7 +35,7 @@ module Logdna
         level: opts[:level],
         env: opts[:env],
         meta: opts[:meta],
-        timestamp: Time.now.to_i
+        timestamp: Time.now.to_i,
       }
       processed_message.delete(:meta) if processed_message[:meta].nil?
       processed_message
