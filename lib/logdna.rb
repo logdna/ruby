2871c0e5c0198fd7d0a9d53bf2007da71174f507
vilyapilya <13.veter@gmail.com>
2019-09-30 15:59:33 -0700

    change the locking logic ang change linting rules

--- lib/logdna.rb
+++ lib/logdna.rb
@@ -46,7 +46,7 @@ module Logdna
       }
     end
 
-    def level=(value)
+    def assign_level=(value)
       if value.is_a? Numeric
         @level = Resources::LOG_LEVELS[value]
         return
@@ -126,9 +126,11 @@ module Logdna
     end
 
     def close
-      if defined? @client and !@client.nil?
-          @client.exitout
-      end
+      @client.exitout if defined? @client && !@client.nil?
+    end
+
+    def at_exit
+      @client.exitout if defined? @client && !@client.nil?
     end
   end
 end
