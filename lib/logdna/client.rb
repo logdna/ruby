df4943c1257b3284d8afeab44ae65f392bb48ccf
vilyapilya <13.veter@gmail.com>
2019-10-24 17:19:39 -0700

    dss

--- lib/logdna/client.rb
+++ lib/logdna/client.rb
@@ -82,6 +82,7 @@ module Logdna
 
     # This method has to be called with @lock
     def send_request
+      puts "lll"
       @side_message_lock.synchronize do
         @buffer.concat(@side_messages)
         @side_messages.clear
