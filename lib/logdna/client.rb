46efe7cd489a9ff15c583680407e8f53436adcac
vilyapilya <13.veter@gmail.com>
2019-12-20 16:52:59 -0800

    remove the automatically added freexes

--- lib/logdna/client.rb
+++ lib/logdna/client.rb
@@ -99,7 +99,6 @@ module Logdna
         ) do |http|
           http.request(@request)
         end
-
         if @response.is_a?(Net::HTTPForbidden)
           @internal_logger.debug("Please provide a valid ingestion key")
         elsif !@response.is_a?(Net::HTTPSuccess)
